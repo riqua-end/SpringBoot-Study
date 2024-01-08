9bdfc3e136951e7331ad5c1f6a0511252f4aa9ca
Kim KJ <riqua1@naver.com>
2024-01-08 13:49:58 +0900

    SpringBoot study - object Mapper

--- src/main/java/com/example/rest/model/UserRequest.java
+++ src/main/java/com/example/rest/model/UserRequest.java
@@ -1,5 +1,6 @@
 package com.example.rest.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserRequest {
 
+//    @JsonProperty("user_names") 특별한 프로퍼티를 만들때 사용
     private String userName;
     private Integer userAge;
     private String email;
